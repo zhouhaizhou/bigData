27bd750b81cac33fd3c1ce6ae68f455ba560616f
cloudWK <1553317586@qq.com>
2018-09-20 18:08:36 +0800

    '修改锚点动画点击太快的原因20180920王斌'

--- src/store/state.js
+++ src/store/state.js
@@ -12,5 +12,6 @@ module.exports = {
   selectedTime:null,   //在线展示页面右边选择的时间索引
   playInterval:null,
   changeUser:false,   //判断用户是否用另一个账户登录，如果变化，怎需要重新拉去权限,false表示没有更改
-  loginTxt:'登录'
+  funReturn:false,   //数据下载锚点定位的时候点击下一个的时候上次动画还没执行完，设置函数停止执行，开始本次动画
+  anchorTimer:null
 }
